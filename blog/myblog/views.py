d728d49bb430660e154e4195009e2587adb8e017
bd240897 <bd240897@yandex.ru>
2022-01-02 19:12:42 +0300

    Сделал раздел - страница поста (пункт 5.7. закончен).

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Post
 from django.core.paginator import Paginator
 
 class MainView(View):
+    """Вьюха главной страницы"""
     # def get(self, request, *args, **kwargs):
     #     return render(
     #         request,
@@ -27,3 +28,11 @@ class MainView(View):
         return render(request, 'myblog/home.html', context={
             'page_obj': page_obj
         })
+
+class PostDetailView(View):
+    """Вьюха поста"""
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myblog/post_detail.html', context={
+            'post': post
+    })
