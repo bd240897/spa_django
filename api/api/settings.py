0dc3cebd649cac8b6714c003d3e401d600bca769
bd240897 <bd240897@yandex.ru>
2022-10-26 00:09:01 +0300

    CORS_ALLOWED_ORIGINS v4

--- api/api/settings.py
+++ api/api/settings.py
@@ -211,8 +211,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://192.168.37.5:8080',
+    'http://localhost:5000',
     'http://62.84.116.230:5000',
 ]
 
 # # what is it?
-CORS_ORIGIN_WHITELIST = ["http://localhost:8000", "http://127.0.0.1:8000", 'http://62.84.116.230:5000',]
+CORS_ORIGIN_WHITELIST = ["http://localhost:8000", "http://127.0.0.1:8000", 'http://localhost:5000', 'http://62.84.116.230:5000',]
