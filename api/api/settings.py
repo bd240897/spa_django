7c93c9e936009d8aa52ab5675da345324dbfa569
bd240897 <bd240897@yandex.ru>
2022-10-25 23:56:28 +0300

    CORS_ALLOW_ALL_ORIGINS

--- api/api/settings.py
+++ api/api/settings.py
@@ -204,13 +204,13 @@ EMAIL_HOST_PASSWORD = 'Пароль который вы только тчо по
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-# https://www.youtube.com/watch?v=A4SrKBLXg_Q
-# https://stackoverflow.com/questions/35760943/how-can-i-enable-cors-on-django-rest-framework
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080',
-    'http://127.0.0.1:8080',
-    'http://192.168.37.5:8080',
-]
+# # https://www.youtube.com/watch?v=A4SrKBLXg_Q
+# # https://stackoverflow.com/questions/35760943/how-can-i-enable-cors-on-django-rest-framework
+# CORS_ALLOWED_ORIGINS = [
+#     # 'http://localhost:8080',
+#     # 'http://127.0.0.1:8080',
+#     # 'http://192.168.37.5:8080',
+# ]
 
-# what is it?
-CORS_ORIGIN_WHITELIST = ["http://localhost:8000", "http://127.0.0.1:8000"]
+# # what is it?
+# CORS_ORIGIN_WHITELIST = ["http://localhost:8000", "http://127.0.0.1:8000"]
