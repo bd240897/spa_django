11c62b0eb53d49473021bf1806e295a58abc426e
bd240897 <bd240897@yandex.ru>
2022-10-28 00:32:04 +0300

    try fixed url added "back"

--- blog_nuxt_v1/store/index.js
+++ blog_nuxt_v1/store/index.js
@@ -1,4 +1,4 @@
 export const state = () => ({
-    baseURL: 'http://localhost:8000', // 'http://178.154.241.157:8000', //'http://localhost:8000',
+    baseURL: 'http://back:8000', // 'http://178.154.241.157:8000', //'http://localhost:8000',
 })
 
