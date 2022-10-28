391a0fdce0b3a8d72b6c90d2c294f8ff9de02276
bd240897 <bd240897@yandex.ru>
2022-10-28 13:47:59 +0300

    fixed URL v3

--- blog_nuxt_v1/store/index.js
+++ blog_nuxt_v1/store/index.js
@@ -1,4 +1,4 @@
 export const state = () => ({
-    baseURL: 'http://62.84.117.17:8000', // process.env.NUXT_API_URL || 'http://localhost:8000', // 'http://back:8000', // 'http://178.154.241.157:8000', //'http://localhost:8000',
+    baseURL: process.env.NUXT_ENV_API_URL || 'http://localhost:8000', // 'http://back:8000', // 'http://178.154.241.157:8000', //'http://localhost:8000',
 })
 
